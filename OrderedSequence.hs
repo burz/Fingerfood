31d6846dd5e8564e227e035d142077c971011d86
Anthony Burzillo <burz@jhu.edu>
2013-08-01 17:42:51 -0400

    OrderedSequence: Initial commit

---
+++ OrderedSequence.hs
@@ -0,0 +1,50 @@
+module OrderedSequence
+( OrderedSequence
+, toOrderedSequence
+, partition
+, insert
+, deleteAll
+, merge
+) where
+
+import Data.Monoid
+
+import FingerTree
+
+data Key a = NoKey | Key a deriving (Eq, Ord)
+
+instance Monoid (Key a) where
+    mempty            = NoKey
+    k `mappend` NoKey = k
+    _ `mappend` k     = k
+
+newtype Elem a = Elem{ getElem :: a }
+
+newtype OrderedSequence a = OrderedSequence (FingerTree (Key a) (Elem a))
+
+instance Measured (Elem a) (Key a) where
+    norm (Elem a) = Key a
+
+toOrderedSequence :: (Ord a) => [a] -> OrderedSequence a
+toOrderedSequence xs = OrderedSequence $ toTree (map Elem xs)
+
+partition :: (Ord a) => a -> OrderedSequence a -> (OrderedSequence a, OrderedSequence a)
+partition k (OrderedSequence xs) = (OrderedSequence l, OrderedSequence r)
+    where (l, r) = split (>= Key k) xs
+
+insert :: (Ord a) => a -> OrderedSequence a -> OrderedSequence a
+insert x (OrderedSequence xs) = OrderedSequence $ l >< (Elem x <| r)
+    where (l, r) = split (>= Key x) xs
+
+deleteAll :: (Ord a) => a -> OrderedSequence a -> OrderedSequence a
+deleteAll x (OrderedSequence xs) = OrderedSequence $ l >< r'
+    where (l, r)  = split (>= Key x) xs
+          (_, r') = split (> Key x) r
+
+merge :: (Ord a) => OrderedSequence a -> OrderedSequence a -> OrderedSequence a
+merge (OrderedSequence xs) (OrderedSequence ys) = OrderedSequence $ merge' xs ys
+    where merge' as bs = case viewL bs of
+            NilL -> as
+            ConsL a bs' -> l >< (a <| merge' bs' r)
+                where (l, r) = split (> norm a) as
+
