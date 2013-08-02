3e70d6dbd09757796caaded20ec979d7971ed77f
Anthony Burzillo <burz@jhu.edu>
2013-08-02 18:10:34 -0400

    IntervalSet: Fix style

--- IntervalSet.hs
+++ IntervalSet.hs
@@ -45,12 +45,12 @@ greater k (n, _) = n > Key k
 intervalSearch :: (Ord a) => IntervalSet a -> Interval a -> Maybe (Interval a)
 intervalSearch (IntervalSet t) i
     | atLeast (low i) (norm t) && low x <= high i = Just x
-    | otherwise                                      = Nothing
+    | otherwise                                   = Nothing
     where Split _ x _ = splitTree (atLeast (low i)) mempty t
 
 intervalMatch :: (Ord a) => IntervalSet a -> Interval a -> [Interval a]
 intervalMatch (IntervalSet t) i = matches $ takeUntil (greater $ high i) t
     where matches xs = case viewL $ dropUntil (atLeast (low i)) xs of
-                          NilL -> []
+                          NilL        -> []
                           ConsL x xs' -> x : matches xs'
 
