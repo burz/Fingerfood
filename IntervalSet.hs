5bc466490a526f278884ea404820a64f797c4ae6
Anthony Burzillo <burz@jhu.edu>
2013-08-02 01:16:49 -0400

    IntervalSet: Finish basic functionality

--- IntervalSet.hs
+++ IntervalSet.hs
@@ -1,16 +1,16 @@
 module IntervalSet
-(
+( Interval(..)
+, IntervalSet
+, intervalSet
+, intervalSearch
+, intervalMatch
 ) where
 
 import Data.Monoid
 
 import FingerTree
 
-instance (Monoid a, Monoid b) => Monoid (a, b) where
-    mempty = (mempty, mempty)
-    (a, b) `mappend` (a', b') = (a `mappend` a', b `mappend` b')
-
-data Interval a = Interval{ low :: a, high :: a }
+data Interval a = Interval{ low :: a, high :: a } deriving Show
 
 data Key a = NoKey | Key a deriving (Eq, Ord)
 
@@ -27,10 +27,14 @@ instance (Ord a) => Monoid (Prio a) where
     p `mappend` MInfty      = p
     Prio m `mappend` Prio n = Prio $ m `max` n
 
-newtype IntervalSet a = IntervalSet (FingerTree (Key (Num a), Prio (Num a)) Interval a)
+newtype IntervalSet a = IntervalSet (FingerTree (Key a, Prio a) (Interval a))
 
-instance Measured Interval (Key (Num a), Prio (Num a)) where
+instance (Ord a) => Measured (Interval a) (Key a, Prio a) where
     norm i = (Key $ low i, Prio $ high i)
+
+intervalSet :: (Ord a) => [(a, a)] -> IntervalSet a
+intervalSet xs = IntervalSet $ toTree (map interval xs)
+    where interval (l, h) = Interval l h
 
 atLeast :: (Ord a) => a -> (Key a, Prio a) -> Bool
 atLeast k (_, n) = Prio k <= n
@@ -38,5 +42,15 @@ atLeast k (_, n) = Prio k <= n
 greater :: (Ord a) => a -> (Key a, Prio a) -> Bool
 greater k (n, _) = n > Key k
 
+intervalSearch :: (Ord a) => IntervalSet a -> Interval a -> Maybe (Interval a)
+intervalSearch (IntervalSet t) i
+    | atLeast (low i) (norm t) && low x <= high i = Just x
+    | otherwise                                      = Nothing
+    where Split _ x _ = splitTree (atLeast (low i)) mempty t
 
+intervalMatch :: (Ord a) => IntervalSet a -> Interval a -> [Interval a]
+intervalMatch (IntervalSet t) i = matches $ takeUntil (greater $ high i) t
+    where matches xs = case viewL $ dropUntil (atLeast (low i)) xs of
+                          NilL -> []
+                          ConsL x xs' -> x : matches xs'
 
