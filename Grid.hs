32f1f1ea24984e1df4da53109d8f961a283f0bd0
Anthony Burzillo <burz@jhu.edu>
2013-08-11 17:58:37 -0400

    Grid: Make grid a subclass of Show

--- Grid.hs
+++ Grid.hs
@@ -3,7 +3,6 @@ module Fingerfood.Grid
 , Position(..)
 , grid
 , toGrid
-, printGrid
 , getList
 , getPosition
 , setPosition
@@ -17,15 +16,14 @@ class Position a where
 type    Column a = Sequence a
 newtype Grid a   = Grid (Sequence (Column a))
 
+instance (Show a) => Show (Grid a) where
+    show (Grid s) = foldr1 (\a b -> a ++ '\n' : b) $ map show (getList s)
+
 grid :: (Position a) => Int -> Int -> Grid a
 grid x y = Grid . sequence' $ take y $ repeat . sequence' $ take x $ repeat empty
 
 toGrid :: [[a]] -> Grid a
 toGrid xss = Grid . sequence' $ map sequence' xss
-
-printGrid :: (Show a) => Grid a -> IO ()
-printGrid (Grid c) = mapM_ printRow (getList c)
-    where printRow = putStrLn . show . getList
 
 getPosition :: Grid a -> Int -> Int -> Maybe a
 getPosition (Grid c) x y = c ! y >>= (! x)
@@ -33,4 +31,14 @@ getPosition (Grid c) x y = c ! y >>= (! x)
 setPosition :: Grid a -> Int -> Int -> a -> Grid a
 setPosition (Grid cs) x y p = Grid $ set cs x c
     where Just c = cs ! y >>= \s -> Just $ set s x p
+
+type    RelativeDimensions = (Int, Int, Int, Int)
+newtype Subgrid a          = Subgrid (Sequence (Maybe (Column (Maybe a))))
+
+instance (Show a) => Show (Subgrid a) where
+    show (Subgrid s) = foldr1 (\a b -> a ++ '\n' : b) $ map show (getList s)
+
+subgrid :: Grid a -> Int -> Int -> RelativeDimensions -> Subgrid a
+subgrid (Grid s) x y (x1, x2, y1, y2) = Subgrid $ subsequence s' (y - y1) (y + y2)
+    where s' = sequence' $ map (\s -> subsequence s (x - x1) (x + x2)) (getList s)
 
